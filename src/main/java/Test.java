957baf40619ff66c257313c6f5675a2d36f57f1e
zhaoyangxzz123 <18217747921@139.com>
2022-03-08 10:13:53 +0800

    test

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,6 +1,6 @@
 public class Test {
     public static void main(String args[])
     {
-        System.out.println("hello worldtest1234");
+        System.out.println("hello worldtest12345");
     }
 }
